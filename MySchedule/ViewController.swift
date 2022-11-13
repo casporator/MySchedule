3db90e4acc89b3504405ebbac02048355c5bea68
Oleg Popov <108875963+casporator@users.noreply.github.com>
2022-11-13 23:19:32 +0100

    Initial Commit

---
+++ MySchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySchedule
+//
+//  Created by Oleg Popov on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
